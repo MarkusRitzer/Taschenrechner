be80a4e631089b69127413efd08ae2ac2b576dc8
Markus Ritzer <markus_ritzer@gmx.de>
2018-02-22 20:59:14 +0100

    Erster Summand wird eingelesen

---
+++ Taschenrechner1/Taschenrechner1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner1
+{
+    class Program
+    {
+        // Es werden zwei Summanden miteinander addiert
+        static void Main(string[] args)
+        {
+            Console.Write("Gib bitte den ersten Summanden ein:");
+            Console.ReadLine();
+        }
+    }
+}
