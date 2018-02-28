82399f9977e9229917ff1995a30a48b02cfea1b4
Markus Ritzer <markus_ritzer@gmx.de>
2018-02-28 16:10:12 +0100

    Variablen und Methodenaufrufe verallgemeinert in Vorbereitung auf die Einführung der Subtraktion

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -8,23 +8,24 @@ namespace Taschenrechner
 {
     class   Program
     {
+  
         static void Main(string[] args)
         {
             // User Story: Als Benutzer möchte ich zwei Zahlen eingeben und die Summe berechenen lassen
-            Console.Write("Gib bitte den ersten Summanden ein:");
-            string ersterSummand = Console.ReadLine();
-            Console.Write("Gib bitte den zweiten Summanden ein:");
-            string zweiterSummand=Console.ReadLine();
+            // Beide Summanden einlesen
+            string ersteZahlalsString= Benutzereingabe("Gib bitte die erste Zahl ein: ");
+            string zweiteZahlalsString = Benutzereingabe("Gib bitte die zweite Zahl ein: ");
 
             //Wandel Text in Zahl
-            double ersterSummandalsZahl = Convert.ToDouble(ersterSummand);
-            double zweiterSummandalsZahl = Convert.ToDouble(zweiterSummand);
+            //TODO: Auslagern wenn Struktur umfangreicher geworden ist
+            double ersteZahl = Convert.ToDouble(ersteZahlalsString);
+            double zweiteZahl = Convert.ToDouble(zweiteZahlalsString);
 
             //Summe bilden
-            double Summe = Addiere(ersterSummandalsZahl,zweiterSummandalsZahl);
+            double Summe = Addiere(ersteZahl,zweiteZahl);
 
             //Ausgabe
-            Console.WriteLine("Die Summe aus {1} und {2} ist: {0:F}", Summe, ersterSummand, zweiterSummand);
+            Console.WriteLine("Die Summe aus {1} und {2} ist: {0:F}", Summe, ersteZahl, zweiteZahl);
             WarteaufBenutzerEingabe();
         }
 
@@ -36,10 +37,26 @@ namespace Taschenrechner
 
         }
 
+        static double Subtrahiere(double minuend, double subtrahent)
+        {
+            double subraktion = minuend - subtrahent;
+            return subraktion;
+
+        }
+
         static void WarteaufBenutzerEingabe()
         {
             Console.WriteLine("Zum Beenden bitte RETURN drücken");
             Console.ReadLine();
         }
+
+        static string Benutzereingabe(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string eingabe= Console.ReadLine();
+
+            return eingabe;
+        }
+
     }
 }
